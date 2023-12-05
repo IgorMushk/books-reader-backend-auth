07f828c9f818e98656103caaa11be71b8a9d5e97
IgorMushk <igor.mushketik@gmail.com>
2023-12-05 18:58:03 +0200

    token = jwt.sign

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,9 @@ const {User} = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const {SECRET_KEY} = process.env;
 
 const register = async (req, res) => {
     const {email, password} = req.body;
@@ -33,7 +36,11 @@ const login = async (req,res) => {
         throw HttpError(401, "Email or password invalid")
     }
 
-    const token = "frfeqeroqijr89-47i987qefqf";
+    const payload = {
+        id: user._id,
+    }
+    
+    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"})
 
     res.json({
         token,
